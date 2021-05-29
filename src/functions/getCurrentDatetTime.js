6f6f8a6986a468254b14cbcaed2beaad42899495
Jose Antonio Gonzalez <jagonzalr@gmail.com>
2021-05-29 11:42:14 +0200

    Add lambda code

---
+++ src/functions/getCurrentDatetTime.js
@@ -0,0 +1,20 @@
+import moment from 'moment'
+
+export const handler = async event => {
+  const currentDatetTime = moment().format('LLLL')
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ currentDatetTime }),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+      'Cache-Control': 'no-cache; no-store',
+      'Content-Type': 'application/json',
+      'Content-Security-Policy': 'default-src self',
+      'Strict-Transport-Security': 'max-age=31536000; includeSubDomains',
+      'X-Content-Type-Options': 'nosniff',
+      'X-XSS-Protection': '1; mode=block',
+      'X-Frame-Options': 'DENY'
+    }
+  }
+}
